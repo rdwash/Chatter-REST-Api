9cf8a90f5a7683d54318d09664fe84b1f61210f2
Rob Washington <rd.wash1@gmail.fake>
2017-08-22 10:33:46 -0500

    added owin support. added entity framework. added system linq dynamic. created owin startup file and added webapi to the owin pipeline. added marvin.jsonpatch for patch updates, if necessary.

---
+++ ChatterApi/ChatterApi.API/App_Start/WebApiConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+
+namespace ChatterApi.API
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            config.MapHttpAttributeRoutes();
+
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/json-patch+json"));
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/vnd.api+json"));
+
+            // Web API routes
+            config.Routes.MapHttpRoute(
+                name: "DefaultRouting",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ ChatterApi/ChatterApi.API/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ChatterApi.API.Startup))]
+
+namespace ChatterApi.API
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+
+            app.UseWebApi(config);
+        }
+    }
+}
