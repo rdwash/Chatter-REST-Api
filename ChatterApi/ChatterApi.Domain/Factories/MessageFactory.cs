c2fe0cde4bb5d138d381414cfe528293f19249cb
Rob Washington <rd.wash1@gmail.fake>
2017-08-22 09:51:32 -0500

    created simple factory classes messages and users.

---
+++ ChatterApi/ChatterApi.Domain/Factories/MessageFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChatterApi.Domain.Entities;
+
+namespace ChatterApi.Domain.Factories
+{
+    public class MessageFactory
+    {
+        public MessageFactory()
+        {
+
+        }
+
+        public DTO.Message CreateMessage(Message message)
+        {
+            return new DTO.Message()
+            {
+                Id = message.Id,
+                ChatMessage = message.ChatMessage,
+                Type = message.Type,
+                UserId = message.UserId
+            };
+        }
+
+        public Message CreateMessage(DTO.Message message)
+        {
+            return new Message()
+            {
+                Id = message.Id,
+                ChatMessage = message.ChatMessage,
+                Type = message.Type,
+                UserId = message.UserId
+            };
+        }
+    }
+}
---
+++ ChatterApi/ChatterApi.Domain/Factories/UserFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChatterApi.Domain.Entities;
+
+namespace ChatterApi.Domain.Factories
+{
+    public class UserFactory
+    {
+        public UserFactory()
+        {
+
+        }
+
+        public DTO.User CreateUser(User user)
+        {
+            return new DTO.User()
+            {
+                Id = user.Id,
+                Type = user.Type,
+                Username = user.Username
+            };
+        }
+
+        public User CreateUser(DTO.User user)
+        {
+            return new User()
+            {
+                Id = user.Id,
+                Type = user.Type,
+                Username = user.Username
+            };
+        }
+    }
+}
